b731fa6a6325271768911ab1f8e8d51efa6615f0
DanielPiskorz <eel5426@gmail.com>
2017-08-04 20:12:44 +0200

    Authentication configuration.

--- src/main/java/pl/danielpiskorz/restfulsecurity/SecurityConfig.java
+++ src/main/java/pl/danielpiskorz/restfulsecurity/SecurityConfig.java
@@ -1,5 +1,30 @@
 package pl.danielpiskorz.restfulsecurity;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception{
+		http
+		.authorizeRequests()
+		.antMatchers("/public/*/").hasRole("USER")
+		.and().formLogin();
+	}
+	
+	@Override
+	public void configure
+		(AuthenticationManagerBuilder auth) throws Exception{
+		auth
+		.inMemoryAuthentication()
+		.withUser("user").password("password").roles("USER")
+		.and()
+		.withUser("daniel").password("password").roles("USER", "ADMIN");
+	}
+	
+	
 }
